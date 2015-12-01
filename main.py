cd679aa514e7dafa865374cd00a2d254a29c576b
Joshua Wertheim <joshwertheim@gmail.com>
2015-12-01 02:24:18 -0800

    Refactored quite a bit of code, added config file for specific path to Shazam database.

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-import sqlite3   
+import sqlite3
+from ConfigParser import SafeConfigParser
+
+class Track(object):
+    """docstring for Track"""
+    def __init__(self, artist, trackname, id):
+        self.artist = artist
+        self.trackname = trackname
+        self.id = id 
 
 def main():
-    dbconnection = sqlite3.connect('data/ShazamDataModel.sqlite')
+    tracks = get_query_results()
+    print_tracks(tracks)
+
+def get_db_path():
+    config_parser = SafeConfigParser()
+    config_parser.read('config.txt')
+    dir_path = config_parser.get('options', 'dir')
+    return dir_path
+
+def get_query_results():
+    dir_path = get_db_path()
+    dbconnection = sqlite3.connect(dir_path)
     cursor = dbconnection.cursor()
     results = cursor.execute(
         '''
@@ -12,9 +31,9 @@ def main():
         '''
     )
 
-    print_rows(results)
-
+    tracks = get_tracks(results)
     dbconnection.close()
+    return tracks
 
 def print_rows(rows):
     for row in rows:
@@ -25,6 +44,17 @@ def get_ids(results):
     for id in results:
         ids.add(id[0])
     return list(ids)
+
+def get_tracks(results):
+    tracks = list()
+    for song in results:
+        track = Track(song[0], song[1], song[2])
+        tracks.append(track)
+    return tracks
+
+def print_tracks(tracks):
+    for t in tracks:
+        print t.artist, t.trackname, t.id
 
 if __name__ == '__main__':
     main()
